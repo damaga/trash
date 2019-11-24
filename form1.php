41ce614ce10ef5ddf7dda6fd0e2582fcc778d99d
damaga <ch1t3r@gmail.com>
2019-11-24 14:45:11 +0300

    Update form1.php

--- form1.php
+++ form1.php
@@ -2,7 +2,8 @@
 /*
 I'm starting to learn php
 This is an example of the output of HTML code in the form of functions. At the right moment, I yank at the conclusion of what I need.
-coded by IDDQD
+coded by Ivan Shestopalov aka IDDQD
+mailto: ctf_iddqd@protonmail.com
 */
 
 require_once('templates.php');
