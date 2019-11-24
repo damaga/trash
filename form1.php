d0f2708abbe441d699a20948cb15a03ff026e843
damaga <ch1t3r@gmail.com>
2019-11-24 13:44:53 +0300

    Create form1.php

---
+++ form1.php
@@ -0,0 +1,13 @@
+<?php
+/*
+I'm starting to learn php
+This is an example of the output of HTML code in the form of functions. At the right moment, I yank at the conclusion of what I need.
+coded by IDDQD
+*/
+
+require_once('templates.php');
+echo header_html();
+echo get_form();
+echo footer_html();
+
+?>
